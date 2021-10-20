fb1ab58723d4c80a6eea9664316edea78b42e42f
shbodake <sayalibodake28@gmail.com>
2021-10-20 14:24:44 +0530

    Added: Welcome message is added

---
+++ src/com/blp/employee/EmployeeWageComputation.java
@@ -0,0 +1,7 @@
+package com.blp.employee;
+
+public class EmployeeWageComputation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+}
